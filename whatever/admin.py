8d4e9e248c890fffc7af300c00264518d0f65be4
Seb Bacon <seb.bacon@gmail.com>
2010-08-18 10:34:19 +0100

    show more stuff in admin

--- whatever/admin.py
+++ whatever/admin.py
@@ -16,9 +16,12 @@ class UserAdmin(admin.ModelAdmin):
 class CompetitionAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_date', 'close_date', 'competition_date')
 
-class LeagueAdmin(admin.ModelAdmin):
-    list_display = ('name', 'date', 'owner')
-    search_fields = ('name',)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('position', 'date',)
+
+class LeagueTableAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ordered_teams', 'added')
+    search_fields = ('added',)
 
 class RegistrationProfileAdmin(admin.ModelAdmin):
     list_display = ('email', 'activation_key', 'activated')
@@ -34,8 +37,9 @@ class EmailMessageAdmin(admin.ModelAdmin):
 
 admin.site.register(models.Prediction, PredictionAdmin)
 admin.site.register(models.CustomUser, UserAdmin)
-admin.site.register(models.League, LeagueAdmin)
+admin.site.register(models.LeagueTable, LeagueTableAdmin)
 admin.site.register(models.RegistrationProfile, RegistrationProfileAdmin)
 admin.site.register(models.Team, TeamAdmin)
+admin.site.register(models.Position, PositionAdmin)
 admin.site.register(models.EmailMessage, EmailMessageAdmin)
 admin.site.register(models.Competition, CompetitionAdmin)
